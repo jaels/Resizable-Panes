7adc94d173699cddf3cc464f4c8113f4651eb18f
Yael Stein <yaelst@gmail.com>
2016-09-23 00:39:29 +0200

    final version plus better pics

--- panes.js
+++ panes.js
@@ -1,14 +1,9 @@
 
-
-
-
 (function() {
 
     function move() {
         var x = event.clientX;
         $(".line").css('left', x - 350 + "px");
-
-console.log(x);
 
         if (x<360) {
             x=360;
@@ -19,12 +14,8 @@ console.log(x);
             x=1042;
             $(".general").off("mousemove", move);
         }
-
-//$("#front-pic").css({width: x - 350 + "px"});
-
-$("#front-pic").css("width", x - 350 + "px");
-
-
+        
+        $("#front-pic").css("width", x - 350 + "px");
     }
 
     $(".line").on("mousedown", function(e) {
@@ -37,6 +28,4 @@ $("#front-pic").css("width", x - 350 + "px");
         $('body').off(".move");
 
     });
-
-
 })();
