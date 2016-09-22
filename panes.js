e1bc3558ac89605f7b4bf558a01736b4a9fb5243
Yael Stein <yaelst@gmail.com>
2016-09-22 17:45:32 +0200

    resizable panes

---
+++ panes.js
@@ -0,0 +1,42 @@
+
+
+
+
+(function() {
+
+    function move() {
+        var x = event.clientX;
+        $(".line").css('left', x - 350 + "px");
+
+console.log(x);
+
+        if (x<360) {
+            x=360;
+            $(".general").off("mousemove", move);
+        }
+
+        if (x>1042) {
+            x=1042;
+            $(".general").off("mousemove", move);
+        }
+
+//$("#front-pic").css({width: x - 350 + "px"});
+
+$("#front-pic").css("width", x - 350 + "px");
+
+
+    }
+
+    $(".line").on("mousedown", function(e) {
+        $('body').on("mousemove.move", move);
+        e.preventDefault();
+    });
+
+    $('body').on("mouseup", function() {
+        console.log("up");
+        $('body').off(".move");
+
+    });
+
+
+})();
